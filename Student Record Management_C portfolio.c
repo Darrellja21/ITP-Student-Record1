d1b7178eed39f4674fdde13c214ae087c3d20365
Darrellja21 <darrellja21@gmail.com>
2025-04-24 18:22:33 +0800

    Update Student Record Management_C portfolio.c

--- Student Record Management_C portfolio.c
+++ Student Record Management_C portfolio.c
@@ -1,214 +1,175 @@
-#include<stdio.h> //this is a standard I/O library for function like printf, scanf and among others to work. It is a must for every C language to have this at the first line.
+#include<stdio.h>    //this is a standard I/O function that is a must in all C programmin language as this allows printf and scanf to work
 
-//creating a structure to store the student data such as name, ID & Marks.
-struct Student{
+struct Student{ //creating a structure call student that will be used throughouT the program
 	
-	int id;          // a unique student ID where one student has one unique ID where  other sudents cant have the same.
-	char name[50];  // the student name will be stored into here which has a space for 50 characters
-	float marks;   // when student enter their marks, it will be stored into here. 
-	
+	int id;   //declaring an integer for ID which will be unique and will be use later for searching and deleting data in the files.
+	char name[50];   //declaring a string of 50 charaters for the user to enter their name
+	float marks, GPA;  //decalring a floating point value so that the user can enter their marks and GPA
 	
 };
 
-//creating a function to add whatever that is stored into the structure above to be added into a file
-void addStudent(){
-
-	struct Student s; // creating a  Student structure instance	
+//creating a function to enter the required student data into the file that we will create
+void StudentData(){
 	
-	FILE *fp = fopen("student.txt", "a"); // opening the file in append mode(threr are three which are read(r) whee it only exist for reading, write(w)where if the file exist, the content will be discarded and the file is treated as new, append(a)where if the file exist new data is written to the end of the file)
+	struct Student s;
 	
-	if(fp == NULL){                       // this simple if statement is to chck if the file can be open, if cant, the printf statement will be display out to the user
+	FILE *fp = fopen("students.txt", "a");  //creating and opening a file name students and using the append mode which is one of three modes in file handling 
+	if(fp == NULL){                         //this if statement is to display only if there is an error opening the file in which case there will return
 		
-		printf("Error Opening the file!\n");
+		printf("There was an error opening the file! Try again.");
 		return;
-		
 	}
 	
-	//now we will get the student data and store it into the structure we have created
-	printf("Enter student ID: ");                    //get the student to give an ID in INTEGER form and we store it into the structure
+	printf("Enter ID: ");                            //get the user to enter an number and store it into the structure
 	scanf("%d", &s.id);
 	
-    printf("Enter Student Name: ");                 //get the student name
-    scanf("%s", &s.name);
-    
-    printf("Enter Marks: ");
-    scanf("%f", &s.marks);                          //get the student marks
-    
-    //write the given data to the file created
-    fprintf(fp, "%d \"%s\" %.2f\n", s.id, s.name, s.marks);
-
-    
-    //once append the file we must always close it
-    fclose(fp);
-    printf("All data has been recorded successfully!\n");  //let the user know the data has been recorded into the file without any errors
-    
+	printf("Enter name(only first name is needed):  ");   //get the user to enter their first name and store it into the structure
+	scanf("%s", &s.name);
+	
+	printf("Enter marks(in two decimal point): ");         //get the user to enter their marks in two decimal point and store it into the structure
+	scanf("%f", &s.marks);
+	
+	printf("Enter GPA(in two decimal point): ");           // get the user to enter thei GPA in two decimal points and store it into the structure
+	scanf("%f", &s.GPA);
+	
+	//now we will want to write to the file without quotes ""
+	fprintf(fp, "%d %s %.2f %.2f\n", s.id, s.name, s.marks, s.GPA);
+	
+	fclose(fp);                       // always close the file once being used to not cause buffer in the file
+	printf("Data has been recorded succesfully!\n");         //display to the user that the data has been recorded into the file
 	
 }
 
-//creating a function to display whatever that is stored into the file
-void viewStudents(){
-	
-	struct Student s;                            // creating a temporary structure to read the data
-	
-	FILE *fp = fopen("student.txt", "r");        // as mentioned above there are three primarily mode to open the file and now we are gonna open it in reading mode where we can only read the data but we cannot change any data or add any data.
-	
-	if(fp == NULL){
-		
-		printf("There is no record inside the file\n");     //similar like in the addStudent function we create an IF statement but this time if there is no record found inside the file, the user will see the printf statement
-		return;
-		
-	}
-	
-	// print headers
-	printf("\n%-10s %-30s %-10s\n", "ID", "Name", "Marks");
-	printf("---------------------------------------------------\n");
-	
-	//now we will ask the program to read each student from the file and print it out using a while loop
-
-     char line[100];
-	while(fgets(line, sizeof(line), fp)) {
-    	if (sscanf(line, "%d \"%[^\"]\" %f", &s.id, s.name, &s.marks) == 3) {
-        	printf("%-10d %-30s %-10.2f\n", s.id, s.name, s.marks);
-    }
-}
-
-	   
-	   fclose(fp);                  //as mentioned above close the file after using
-	   
-}
-
-//this function will help to search for a student using ID
-void searchStudent(){
-	
-	int searchID;  //declare a variable that we will use to search for the student later
-    
-	struct Student s;
-	int found = 0;   //	this is to check if there was a record that was found
-	
-	FILE *fp = fopen("student.txt", "r");  //similar to the above function we will be opening the file and in read mode
-    
-    if(fp == NULL)
-    {
-    	
-    	printf("There is an error trying to open the file\n");
-    	return;
-    	
-	}
-	
-	 printf("Enter student ID to search: ");        //the program will now ask the user to enter the ID of the student that want to be searched
-    scanf("%d", &searchID);
-    
-    //here we will find for the same match and display for ID the user is looking for using a while loop with two nested if statement. At the end the break statement will tell the program to stop after finding a match.
-    
-  char line[100]; 
-		while(fgets(line, sizeof(line), fp)) {
-    		if (sscanf(line, "%d \"%[^\"]\" %f", &s.id, s.name, &s.marks) == 3) {
-        		if (s.id == searchID) {
-            		printf("Student found:\n");
-            		printf("%-10s %-30s %-10s\n", "ID", "Name", "Marks");
-            		printf("%-10d %-30s %-10.2f\n", s.id, s.name, s.marks);
-            		found = 1;
-            		break; // stop after finding
-        }
-    }
-}
-
-
-	
-	//this is statement will display if the ID that the user has entered does not match with any of the ID in the file
-	if(!found){
-		
-		
-		printf("Student with ID %d not found.\n", searchID);
-		
-	}
-	
-	fclose(fp);   //As we have done with the previous funtion, close the file once we are done
-}
-
-	
-	
-	//this last function is to delete a student record by ID
-	
-	void deleteStudent(){
-		
-		int DelID, found = 0;   /*declare a variable that we will use to delete for the student later
-		                          check if there was a record that was found*/
+	//function to view and search student based on ID
+	void AddStudent(int searchid){
 		
 		struct Student s;
 		
-		//open the file to do two things which are to read records and create a temporary file to copy non-deleted records 
-		FILE *fp = fopen("student.txt", "r");
-		FILE *temp = fopen("temp.txt", "w");
-		
-		if(fp == NULL || temp == NULL ){
+		FILE *fp = fopen("students.txt", "r");  // open the file like above but this time in read mode
+		if(fp == NULL){
 			
-			printf("There is an error trying to open the file!\n");
-			return;
-			
+			printf("There was an error opening the file! Try again.");
+			return ;
 		}
 		
-		//now we will read the ID that wants to be deleted by the user
-		printf("Enter student ID to delete: ");
-		scanf("%d", &DelID);
+		int found = 0;
 		
-		/* this while loop has a nested if and also an if-else loop is to copy only the non-matching student records into the temp file we have created  */
-	char line[100]; 
-	while(fgets(line, sizeof(line), fp)) {
-    	if (sscanf(line, "%d \"%[^\"]\" %f", &s.id, s.name, &s.marks) == 3) {
-        	if (s.id == DelID) {
-            	found = 1; // mark as found, and don't write this to temp
-        	} else {
-            		fprintf(temp, "%d \"%s\" %.2f\n", s.id, s.name, s.marks);
-        }
-    }
-}
-
-
+		printf("\n%-10s %-30s %-10s %-10s\n", "ID", "name", "marks", "GPA");             /* what this two printf statement does is that it print out the data from the file */
+		printf("-------------------------------------------------------------------\n");
 		
-		fclose(fp);            // as usual close the file
-		fclose(temp);          // close the temporary file
+		//this while loop that has a nested if statement inside is to read and display out the data from the file if there is a same
+		while(fscanf(fp, "%d %s %f %f", &s.id, s.name, &s.marks, &s.GPA)==4)
+		{
+			if(searchid == -1|| s.id==searchid){
+				
+				printf("%-10d %-30s %-10.2f %-10.2f\n", s.id, s.name, s.marks, s.GPA );
+				found =1;
+			}
+		}
 		
-		remove("student.txt");   //delete the original file
-		rename("temp.txt", "student.txt");  //rename the temporary file to the original file
+		fclose(fp);    //as the above function, close the file one use is complete
 		
-		if(found){
+		if(searchid != -1  && !found){
 			
-			printf("Student record deleted successfully. \n");
-		}else{
-			printf("The student ID is not found.\n");
+			printf("Student with ID %d could not be found.\n", searchid);
 		}
 		
 	}
 	
-	//main function is a must in all C programming language
+	//this function is to delete the data in the file using ID
+	void deleteStudent(){
+
+int DelID, found = 0;                //declare two integer varible to delete the data and set the found counter to 0 so it does not cause any error
+		
+		struct Student s;
+		
+		FILE *fp = fopen("students.txt", "r"); //this two lines are to open the original file in read mode but we will also be opening a new file
+		FILE *temp = fopen("temp.txt", "w");  
+		
+		//this if statement is the same as before to tell the user that there is a problem opning the file but this time we are including the new file we have created just for this function
+		if(fp == NULL || temp == NULL){
+			
+			printf("There was an error opening the file! Try again.");
+			return ;
+		}
+		
+		//ask the user which ID they would like to delete
+		printf("Enter an ID to delete: ");
+		scanf("%d", &DelID);
+		
+		while(fscanf(fp, "%d %s %f %f", &s.id, s.name, &s.marks, &s.GPA)==4){
+			
+			if(s.id == DelID){
+				
+				found = 1;
+			}else{
+				
+				fprintf(temp, "%d %s %.2f %.2f\n",s.id, s.name, s.marks, s.GPA );
+			}
+		}
+		
+		fclose(fp);
+		fclose(temp);
+		
+	     remove("students.txt");
+	     rename("temp.txt", "students.txt");
+		
+		if(found){
+			
+			printf("Data has been deleted succecfully deleted.\n");
+		}else{
+			
+			printf("The student ID could not be found.\n");
+		}
+	}		
+
+	
+	
+	
+	
+	
+	
+
+
+//main function the most essential and important  function in the C Language 
 int main(){
+
+int choice; //Declaring an integer variable to capture the user's choice
+
+//when the program runs, below is what the user will see first
+do{
 	
-	int choice;   //This variable is created to store user's  menu choice
+	printf("----Student Management Record System----\n");
+	printf("1. Add Student\n");
+	printf("2. Search/View student\n");
+	printf("3. Delete Student\n");
+	printf("4. Exit\n");
+	printf("Enter your choice: ");
+	scanf("%d", &choice);
 	
-	//we will now do a do-while loop that has a nested switch case inside to print out the menu options and execute the function based on the user's choice
-   do{
-   	
-   	printf("\n--- Student Record Management System ---\n");
-   	printf("1. Add Student\n");
-   	printf("2. View Students\n");
-   	printf("3. Search Student\n");
-   	printf("4. Delete Student\n");
-   	printf("5. Exit\n");
-   	printf("Enter your choice: ");
-   	scanf("%d", &choice);
-   	
-   	switch(choice){
-   		
-   		case 1: addStudent(); break;
-   		case 2: viewStudents(); break;
-   		case 3: searchStudent(); break;
-   		case 4: deleteStudent(); break;
-   		case 5: printf("Exiting...\n"); break;
-   		default: printf("Invalid choice! Try again. \n"); break;
-   		
-	   }
-   	
-   } while( choice != 5);    //this is to tell the program to end the loop if the user exits
-	return 0; //end of program is a must in C programming language
+	switch(choice){
+		
+		case 1: StudentData(); break;
+		
+		case 2: {
+			
+		   	int IDChoice;
+		   	printf("Enter an ID to search a student(or -1 to view all): ");
+		   	scanf("%d", &IDChoice);
+		   	AddStudent(IDChoice);
+			break;
+	}
+				
+		case 3: deleteStudent(); break;
+		
+		case 4: printf("Exiting...\n"); break;
+		
+		default: printf("Invalid Choice! Try again..\n");
+	}
+}while(choice !=4);
+
+
+return 0;
+
+
 }
